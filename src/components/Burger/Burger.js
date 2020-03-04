c6769190f68c93a5807023a77f7e9f851d60f7e4
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-03-04 16:34:03 +0900

    材料の場所を少し変更しましたよ！

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import classes from './Burger.module.css';
 import BugerIngredient from './Burgeringredient/Burgeringredient'
 import BurgerIngredient from './Burgeringredient/Burgeringredient';
 const burger =(props)=>{
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(isKey =>{
         return [...Array(props.ingredients[isKey])].map((_,i)=>{
            return <BurgerIngredient key={isKey +i} type={isKey} />
         })
-    });
+    })
+    .reduce((arr,el)=>{
+        return arr.concat(el)
+    }, []);
+    if (transformedIngredients.length ===0){
+        transformedIngredients=<p>材料を追加してください！なんで追加しないんですか？バカですか？コロナですか？</p>
+    }
+    console.log(transformedIngredients)
     return (
      <div className={classes.Burger}>
           <BurgerIngredient type="bread-top" />

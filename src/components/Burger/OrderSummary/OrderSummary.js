acc221cef7c44bb5c9aa4fecf0bb84621b804ab2
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-03-07 22:16:17 +0900

    ロゴの追加とヘッダーの追加

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Aux from '../../../hoc/Aux'
+import Button from '../../UI/Button/Button'
 const OrderSummary =(props)=>{
     const ingredientSummary = Object.keys(props.ingredients)
     .map(igKey =>{
@@ -9,11 +10,14 @@ const OrderSummary =(props)=>{
  return (
      <Aux>
          <h3>あなたのオーダー</h3>
-         <p>A delecious burger with the following ingredients:</p>
+         <p>あなたの材料は以下の通りよ❤️</p>
          <ul>
             {ingredientSummary}
          </ul>
-         <p>Cotinue to Checkout?</p>
+         <p><strong>会計の値段:{props.price.toFixed(2)}</strong></p>
+         <p>会計しますか？</p>
+         <Button btnType="Danger" clicked={props.purchaseCancelled}>キャンセル</Button>
+         <Button btnType="Success" clicked={props.purchaseContinued}>続ける</Button>
      </Aux>
  )
 }
